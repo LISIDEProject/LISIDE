f2c5698c69c9ed6db3593501e63d5466d363ef9f
Zatherz <zatherz@linux.pl>
2020-02-25 19:03:16 +0100

    Fix building on Arch (F# targets&openssl-1.0 fix)

--- main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/build_libgit2.sh
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/build_libgit2.sh
@@ -20,6 +20,11 @@ fi
 mkdir build
 pushd build
 
+extra_cmake_args=""
+if [ -e "/usr/include/openssl-1.0" ]; then # prefer openssl 1.0 (arch)
+	extra_cmake_args="-DOPENSSL_ROOT_DIR=/usr/lib/openssl-1.0 -DOPENSSL_INCLUDE_DIR=/usr/include/openssl-1.0"
+fi
+
 cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
       -DTHREADSAFE:BOOL=ON \
       -DBUILD_CLAR:BOOL=OFF \
@@ -27,6 +32,7 @@ cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
       -DLIBGIT2_FILENAME=git2-$SHORTSHA \
       -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
       -DCMAKE_SKIP_RPATH=TRUE \
+      $extra_cmake_args \
       ..
 
 cmake --build .
